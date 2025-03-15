f69b9a07d6263d2600b8c8e8dea8e4f2cac90696
Susantha Perera <33259705+mskperera@users.noreply.github.com>
2025-03-15 15:25:50 +0530

    Update lb_media_db.sql

--- lb_media_db.sql
+++ lb_media_db.sql
@@ -60,9 +60,11 @@ CREATE TABLE `images` (
   `folder_path` varchar(255) NOT NULL,
   `created_at` datetime DEFAULT current_timestamp(),
   `is_resized` tinyint(1) DEFAULT 0,
+  `isCommitted` bit(1) DEFAULT b'0',
+  `toBeDeleted` bit(1) DEFAULT b'0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `hash` (`hash`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=115 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -120,4 +122,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-03-13 20:29:11
+-- Dump completed on 2025-03-15 15:13:20
